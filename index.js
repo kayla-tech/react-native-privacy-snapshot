c99eb57d956f3228f42113b4bb9e544a565cefac
Ray Deck <ray@raydeck.com>
2020-03-31 17:25:32 -0500

    Adding usePrivacySnapshot hook

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import { NativeModules, Platform } from "react-native";
 let { PrivacySnapshot } = NativeModules;
 const show = () => Platform.OS === "ios" && PrivacySnapshot._show(true);
 const hide = () => Platform.OS === "ios" && PrivacySnapshot._show(false);
 const enabled = enable =>
   Platform.OS === "ios" && PrivacySnapshot.enabled(enable);
-export { show, hide, enabled };
+const usePrivacySnapshot = () => {
+  useEffect(() => {
+    show();
+    return () => {
+      hide();
+    };
+  }, []);
+};
+export { show, hide, enabled, usePrivacySnapshot };
